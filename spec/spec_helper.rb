651eb035ffa49057d707852832ab87b27160445e
pokopato <patora20140808@gmail.com>
2015-07-13 17:12:32 +0900

    ４章終わったところ

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,26 +1,31 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/autorun'
+require 'rubygems'
+require 'spork'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
 
-# Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+  Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
-RSpec.configure do |config|
+  ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  RSpec.configure do |config|
 
-  config.use_transactional_fixtures = true
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  config.infer_base_class_for_anonymous_controllers = false
+    config.use_transactional_fixtures = true
 
-  config.order = "random"
+    config.infer_base_class_for_anonymous_controllers = false
 
-  config.include Capybara::DSL
+    config.order = "random"
+
+    config.include Capybara::DSL
+  end
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+
 end
